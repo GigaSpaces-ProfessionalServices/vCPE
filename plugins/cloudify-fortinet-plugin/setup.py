f4716ff41be77d37c5be90796fa37934ad90260a
alons@gigaspaces.com <Strikov!!57>
2016-06-23 16:29:17 +0300

    first commit

--- plugins/cloudify-fortinet-plugin/setup.py
+++ plugins/cloudify-fortinet-plugin/setup.py
@@ -14,72 +14,22 @@
 #    * limitations under the License.
 '''Cloudify Fortinet plugin package config'''
 
-from os import environ, path
-import logging
-from subprocess import Popen, PIPE
 from setuptools import setup
-from setuptools.command.install import install
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def install_package(pip_path, pkg):
-    '''Install a required package'''
-    proc = Popen([pip_path, 'install', pkg], stderr=PIPE)
-    err = proc.communicate()
-    if proc.returncode:
-        raise Exception('Installing Python package "{0}" failed: {1}'
-                        .format(pkg, err))
-
-
-def uninstall_package(pip_path, pkg):
-    '''Uninstall a required package'''
-    proc = Popen([pip_path, 'uninstall', pkg, '-y'], stderr=PIPE)
-    err = proc.communicate()
-    if proc.returncode:
-        raise Exception('Uninstalling Python package "{0}" failed: {1}'
-                        .format(pkg, err))
-
-
-def install_requirements():
-    '''Install required Python packages'''
-    logger = logging.getLogger('setup.py')
-
-    ureqs = ['paramiko']
-    # This is needed due to an SSH bug in FortiGate and latest Paramiko (Fabric)
-    reqs = ['https://github.com/01000101/paramiko/archive/v1.17-fortigate.zip']
-
-    pip_win_path = path.join(environ.get('VIRTUAL_ENV'), 'Scripts/pip.exe')
-    pip_nix_path = path.join(environ.get('VIRTUAL_ENV'), 'bin/pip')
-    pip_path = pip_nix_path if path.exists(pip_nix_path) else pip_win_path
-
-    logger.info('Using PIP: %s', pip_path)
-    for ureq in ureqs:
-        logger.info('Uninstalling Python package "%s"', ureq)
-        uninstall_package(pip_path, ureq)
-    for req in reqs:
-        logger.info('Installing Python package "%s"', req)
-        install_package(pip_path, req)
-
-
-class CustomInstall(install):
-    '''Custom install'''
-    def run(self):
-        install_requirements()
-        install.run(self)
-
 
 setup(
     name='cloudify-fortinet-plugin',
+    description='A Cloudify plugin for Fortinet NFV orchestration',
     version='1.0',
     license='LICENSE',
-    packages=[
-        'fortigate',
-        'fortigate.firewall'
-    ],
+    zip_safe=False,
+
+    # This must correspond to the actual packages in the plugin.
+    packages=['fortigate', 'fortigate.firewall'],
     cmdclass={'install': CustomInstall},
-    description='A Cloudify plugin for Fortinet NFV orchestration',
+
     install_requires=[
-        'cloudify-plugins-common>=3.3.1'
+        # Necessary dependency for developing plugins, do not remove!
+        "cloudify-plugins-common>=3.3.1",
+        "paramiko"
     ]
 )
